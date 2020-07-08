1175e1c535eb79e4e4621881965447f00b36e33b
Muhammad Tahseen Mashaidi <mashaidi96@gmail.com>
2020-07-08 18:39:37 +0500

    files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import { ConnectedRouter } from 'connected-react-router';
+import { ConnectedRouter } from 'connected-react-router';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-      {/*<ConnectedRouter history={history}>*/}
+      <ConnectedRouter history={history}>
           <App/>
-      {/*</ConnectedRouter>*/}
+      </ConnectedRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
