3db877ec15ab136319a2caf5e50cd408f5fcdfa6
qasim raheem <qasimmehmood13936@gmail.com>
2020-08-08 18:06:02 +0500

    Header complete without login
    header is completed without user profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import logo from '../assets/images/logo.png'
 import logoCaption from '../assets/images/logocaption.png'
 import '../assets/scss/header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import {Button, ButtonGroup} from '@material-ui/core'
 
 
 class Header extends Component {
@@ -9,17 +11,27 @@ class Header extends Component {
         return (
             <div>
                 <header className="Header">
-                    <a href="#">
-                        <figure className="logo-container">
-                            <img src={logo} alt="Logo" className="Logo"/>
-                            <img src={logoCaption} alt="logo caption" className="LogoCaption"/>
-                        </figure>
-                    </a>
-                    <nav>
-
+                    <figure className="logo-container">
+                        <img src={logo} alt="Logo" className="Logo"/>
+                        <img src={logoCaption} alt="logo caption" className="LogoCaption"/>
+                    </figure>
+                    <nav className="navbar">
+                        <ul>
+                            <li>HOME</li>
+                            <li>SMART CONTRACTS</li>
+                            <li>DAPPS</li>
+                            <li>DOWNLOADS</li>
+                            <li>BLOCK EXPLORER</li>
+                            <li>ABOUT US</li>
+                            <li>HELP</li>
+                        </ul>
                     </nav>
-                    <div>
-
+                    <div className="loginContainer">
+                        <ButtonGroup variant="contained" color="Primary" aria-label="contained primary button group"
+                                     className="loginbtn">
+                            <Button>Register</Button>
+                            <Button>Login</Button>
+                        </ButtonGroup>
                     </div>
                 </header>
             </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            i am home
+            {/*i am home*/}
         </div>
     );
 };
