e96e5aae2890749123075a0caa47d20e226ccd6c
Muhammad Tahseen Mashaidi <mashaidi96@gmail.com>
2020-08-08 16:09:21 +0500

    Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from '../assets/images/logo.png'
 import logoCaption from '../assets/images/logocaption.png'
 import '../assets/scss/header.css'
+import {DappsIcon} from "./ui/Icons";
 
 
 class Header extends Component {
@@ -9,12 +10,10 @@ class Header extends Component {
         return (
             <div>
                 <header className="Header">
-                    <a href="#">
-                        <figure className="logo-container">
-                            <img src={logo} alt="Logo" className="Logo"/>
-                            <img src={logoCaption} alt="logo caption" className="LogoCaption"/>
-                        </figure>
-                    </a>
+                   <DappsIcon
+                       link={true}
+                       linkTo="/"
+                   />
                     <nav>
 
                     </nav>
--- src/components/ui/Icons.js
+++ src/components/ui/Icons.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import logo from '../../assets/Dappslogo.jpg'
+import logo from '../../assets/images/logo.png'
+import logoCaption from "../../assets/images/logocaption.png";
 
 
 export const DappsIcon = (props) => {
-    const template= <div
-        className={"img_cover"}
-        style={{
-            width:props.width,
-            height:props.height,
-            background:`url(${logo}) no-repeat`,
-            backgroundSize:'70px 70px'
-        }}
-    >
+    const template= <div>
+        <a href="#">
+            <figure className="logo-container">
+                <img src={logo} alt="Logo" className="Logo"/>
+                <img src={logoCaption} alt="logo caption" className="LogoCaption"/>
+            </figure>
+        </a>
     </div>;
     if(props.link){
         return(
