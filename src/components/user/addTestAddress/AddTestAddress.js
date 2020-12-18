6e111020158c18fd15ef9fbe3a46ec42535e36d6
Tahseen Mashaidi <mashaidi96@gmail.com>
2020-12-18 22:22:59 +0500

    add test

--- src/components/user/addTestAddress/AddTestAddress.js
+++ src/components/user/addTestAddress/AddTestAddress.js
@@ -1,26 +1,84 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Layout from "../../../hoc/Layout";
+import '../../../assets/scss/add_test_address.css'
+import {Button, Table} from "semantic-ui-react";
+import {useLazyQuery, useMutation, useQuery} from "@apollo/client";
+import {addTestAddress, me_Query} from "../../../queries/queries";
+import {Client} from "../../../queries/Services";
+import {withAlert} from "react-alert";
+import {Spinner2} from "../../ui/Spinner";
+import {flowRight as compose} from "lodash";
+import {connect} from "react-redux";
+import {setUser} from "../../../actions/Actions";
 
 const AddTestAddress = (props) => {
-    const {user}=props;
-    console.log(user)
+    const {alert}=props;
+
+    const [add]=useMutation(addTestAddress,{
+        client:Client,
+        context:{
+            headers: {
+                authorization: localStorage.getItem("token")
+            }
+        },
+        onCompleted:data => {
+            refetch();
+           alert.success("address Generated",{timeout:2000});
+        },onError:error => {
+            alert.error(error.toString(),{timeout:3000})
+        }
+    })
+    const AddAddress=()=>{
+        add().catch(err=>{
+            console.log(err);
+        })
+    }
+    const {loading,data,error,refetch}=useQuery(me_Query,{
+        client:Client,
+        context:{
+            headers: {
+                authorization: localStorage.getItem("token")
+            }
+        },
+        onCompleted:data => {
+            props.setUser(data.me)
+        },onError:error => {
+            alert.error("new"+error.toString(),{timeout:3000})
+        }
+    });
+    if (loading) return <Spinner2/>
+    if (error) return <div>{error.toString()}</div>
     return (
         <Layout>
-            <div >
+            <div className={'add-test'}>
                 <h2>Add Test Address</h2>
-                <tabel>
-                    <tr>
-                        <th>Address</th>
-                        <th>Balance</th>
-                    </tr>
-                    <tr>
-                        <td>fdfgfd</td>
-                        <td>sds</td>
-                    </tr>
-                </tabel>
+                <Table>
+                    <Table.Header>
+                        <Table.Row>
+                        <Table.HeaderCell width={1}>No</Table.HeaderCell>
+                        <Table.HeaderCell>Address</Table.HeaderCell>
+                        <Table.HeaderCell>Balance</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {data.me.testAddress.length>0&&
+                            data.me.testAddress.map((test,index)=>{
+                              return  <Table.Row key={test.id}>
+                                    <Table.Cell>{index+1}</Table.Cell>
+                                    <Table.Cell>{test.address}</Table.Cell>
+                                    <Table.Cell>{test.balance}</Table.Cell>
+                              </Table.Row>
+                            })
+                        }
+                    </Table.Body>
+                </Table>
+                <Button color={"green"} onClick={AddAddress}>Add new Adress</Button>
             </div>
         </Layout>
     );
 };
 
-export default AddTestAddress;
+export default compose(
+    connect(null, {setUser}),
+    withAlert()
+) (AddTestAddress);
