47e2682610a18da79d77c5d80edbf822a729d3a3
Tahseen Mashaidi <mashaidi96@gmail.com>
2020-12-28 11:58:57 +0500

    admin verification bugs remove

--- src/components/home/smartContract/compileSmartContract/CustomizedSmartContract.js
+++ src/components/home/smartContract/compileSmartContract/CustomizedSmartContract.js
@@ -1,8 +1,47 @@
-import React from 'react';
-import {Button, Divider, Form, Item} from "semantic-ui-react";
+import React, { useState} from 'react';
+import "../../../../assets/scss/compile.css"
+import {Button, Container, Divider, Form, Item, Segment} from "semantic-ui-react";
+import Layout from "../../../../hoc/Layout";
+import {flowRight as compose} from "lodash";
+import {compile, licenseById, me_Query} from "../../../../queries/queries";
+import {Spinner2} from "../../../ui/Spinner";
+import { useMutation, useQuery} from "@apollo/client";
+import {connect} from "react-redux";
+import {setUser} from "../../../../actions/Actions";
+import {withAlert} from "react-alert";
+import {Client} from "../../../../queries/Services";
 
-const CustomizedSmartContract = (props) => {
-    const contract=props.contract;
+import CompileLayout from "../../../../hoc/CompileLayout";
+const alphabet=RegExp(/^[a-zA-Z][a-zA-Z\s]*$/);
+
+const Compile =(props)=> {
+    const [licenses, setLicenses] = useState(null);
+    const [name, setName] = useState("");
+    const [Loading,setLoading]=useState(false);
+
+    const [cloading, setCLoading] = useState(false);
+    const alert = props.alert;
+
+    const handleChange = (event) => {
+        const {value} = event.target;
+        if (alphabet.test(value)) {
+            setName(value)
+        }
+        if (value === "") {
+            setName(value)
+        }
+    }
+    const [newCompile] = useMutation(compile, {
+        client: Client,
+        onCompleted: data => {
+
+            setCLoading(false)
+        },
+        onError: error1 => {
+            alert.error(error1.toString(), {timeout: 2000})
+        },
+        refetchQueries: me_Query
+    })
     const returnColor=(color)=>{
         switch (color){
             case "TOOLS":
@@ -21,52 +60,100 @@ const CustomizedSmartContract = (props) => {
                 return "violet";
         }
     }
-    return (
-        <div className={"customize"}>
-            <h2>Customize your contract</h2>
-            <Divider/>
-            <Item.Group>
-                <Item>
-                    <Item.Image src={contract.image}/>
-                    <Item.Content>
-                        <Item.Header>{contract.contractName}</Item.Header>
-                        <Item.Meta >
-                            <span>Published By</span>
-                            <span>{contract.publisher.fullName}</span>
-                            <span>|{contract.publishingDateTime}</span>
-                        </Item.Meta>
-                        <Item.Description>
-                            {contract.shortDescription}
-                        </Item.Description>
-                        <Item.Extra>
-                            <div className={"contract_category"}>
-                                {contract.contractCategory.map((category, index) => {
-                                    return <Button
-                                        disabled  size={"mini"}
-                                        color={returnColor(category)} key={category}>
-                                        {category}
-                                    </Button>
-                                })
-                                }
-                            </div>
-                        </Item.Extra>
-                    </Item.Content>
-                </Item>
-            </Item.Group>
-            <Divider/>
-            <Form>
-                <Form.Field>
-                    <label>Name of this compilation</label>
-                    <Form.Input
-                        fluid onChange={(event)=>props.change(event)}
-                        type={'text'} value={props.name}
-                    />
-                    <p>This will help you differentiate it between multiple compilation</p>
-                </Form.Field>
-            </Form>
-            <Button  className={"compilebtn"} onClick={props.onCompiled}>Compile</Button>
-        </div>
-    )
-};
+    const onCompiled = (liecense) => {
+        setCLoading(true)
+        newCompile({
+            variables: {
+                name: name.toString(),
+                sId: liecense.order.smartContract.id,
+                pId: liecense.purchasedContract.id,
+                lId: liecense.id
+            },
+            context: {
+                headers: {
+                    authorization: localStorage.getItem("token")
+                }
+            }
+        }).catch(err => {
+            console.log(err.toString())
+        });
+    }
+    const RenderData = () => {
+        const {error, loading, data} = useQuery(licenseById, {
+            variables: {id: props.match.params.id},
+            client: Client,
+            onCompleted: data1 => {
+                if (data1.licenseById.used) {
 
-export default CustomizedSmartContract;
+                }
+                setLicenses(data1.licenseById)
+            }
+        })
+        if (loading) return <Spinner2/>
+        if (Loading) return <Spinner2/>
+        if (error) return <div>{error.toString()}</div>
+        if (data) {
+            const liecense = data.licenseById;
+            const contract = license.testOrder.smartContract;
+            return (
+                <div className={"customize"}>
+                    <h2>Customize your contract</h2>
+                    <Divider/>
+                    <Item.Group>
+                        <Item>
+                            <Item.Image src={contract.image}/>
+                            <Item.Content>
+                                <Item.Header>{contract.contractName}</Item.Header>
+                                <Item.Meta>
+                                    <span>Published By</span>
+                                    <span>{contract.publisher.fullName}</span>
+                                    <span>|{contract.publishingDateTime}</span>
+                                </Item.Meta>
+                                <Item.Description>
+                                    {contract.shortDescription}
+                                </Item.Description>
+                                <Item.Extra>
+                                    <div className={"contract_category"}>
+                                        {contract.contractCategory.map((category, index) => {
+                                            return <Button
+                                                disabled size={"mini"}
+                                                color={returnColor(category)} key={category}>
+                                                {category}
+                                            </Button>
+                                        })
+                                        }
+                                    </div>
+                                </Item.Extra>
+                            </Item.Content>
+                        </Item>
+                    </Item.Group>
+                    <Divider/>
+                    <Form>
+                        <Form.Field>
+                            <label>Name of this compilation</label>
+                            <Form.Input
+                                fluid onChange={(event) => props.change(event)}
+                                type={'text'} value={props.name}
+                            />
+                            <p>This will help you differentiate it between multiple compilation</p>
+                        </Form.Field>
+                    </Form>
+                    <Button className={"compilebtn"} onClick={()=>onCompiled()}>Compile</Button>
+                </div>
+                // <CustomizedSmartContract
+                //     change={(event) => handleChange(event)}
+                //     name={name}
+                //     contract={liecense.order.smartContract}
+                //     onCompiled={() => OnComplied(liecense)}
+                // />
+            )
+        }
+        return <div>Not found Retry</div>
+    }
+    return (
+        <CompileLayout type={'test'}>
+            {RenderData()}
+        </CompileLayout>
+    )
+}
+export default compose(connect(null,{setUser}),withAlert())(Compile);
